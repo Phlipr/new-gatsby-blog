89998d2f7bce376e7425c1c9ad52135df9c50da1
Phillip Reynolds <preynolds1406@gmail.com>
2021-12-02 18:50:12 -0600

    Lecture 362: Completed gatsby-blog.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -6,4 +9,43 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+
+  return graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode });
+
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    });
+  }
 }
